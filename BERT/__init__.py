ea6a22cdd2e2b381810dea3ce84522519ffd9400
lif54334 <lif54334@163.com>
2019-05-21 18:48:23 +0800

    initial

---
+++ BERT/__init__.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+
+# -*- encoding: utf-8 -*-
+
+'''
+@Author  :   {lif54334}
+
+@Software:   PyCharm
+
+@File    :   __init__.py.py
+
+@Time    :   2019/5/21 18:42
+
+@Desc    :
+
+'''
+
+import
+
+...
+
+if __name__ == '__main__':
+    pass
